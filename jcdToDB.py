ebc17cb4cfc7eda56972f9933109407e076bdbec
EJEgan <eugene.egan1@ucdconnect.ie>
2021-02-25 11:54:37 +0000

    time & date, plus extra table

--- jcdToDB.py
+++ jcdToDB.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import requests
 import json
+from datetime import datetime
 
 try:
     print("trying to connect")
@@ -21,19 +22,41 @@ try:
 
     r = requests.get(STATIONS, params={"apiKey": APIKEY, "contract": NAME})
 
-    def stations_to_db(text):
+    now = datetime.now()
+    current_time = now.strftime("%H%M") #time as a 3-4 sequence of numbers
+    day = datetime.today().weekday() #produces an int value for day of the week
+    date = now.strftime("%d/%m/%Y") #probably not going to be used computationally, just for our benefit
+
+    def live_to_historical(text):
         stations = json.loads(text)
         for station in stations:
             vals = (int(station.get('number')), station.get('name'),
                     int(station.get('bike_stands')), int(station.get('available_bike_stands')),
                     int(station.get('available_bikes')), station.get('status'),
-                    int(station.get('last_update'))
+                    int(current_time), int(day), date
                     )
-            mycursor.execute("insert into AvailableBikes values(%s,%s,%s,%s,%s,%s,%s)", vals)
+            mycursor.execute("insert into LiveHistoricalData values(%s,%s,%s,%s,%s,%s,%s,%s,%s)", vals)
             mydb.commit()
         return
 
-    stations_to_db(r.text)
+    def replace_live_data(text):
+        mycursor.execute("truncate table AvailableBikes")
+        mydb.commit()
+        stations = json.loads(text)
+        for station in stations:
+            vals = (int(station.get('number')), station.get('name'),
+                    int(station.get('bike_stands')), int(station.get('available_bike_stands')),
+                    int(station.get('available_bikes')), station.get('status'),
+                    int(current_time), int(day), date
+                    )
+            mycursor.execute("insert into AvailableBikes values(%s,%s,%s,%s,%s,%s,%s,%s,%s)", vals)
+            mydb.commit()
+        return
+
+    live_to_historical(r.text)
+    print("Historical DB updated")
+    replace_live_data(r.text)
+    print("Live table updated")
     mydb.close()
 except Exception as e:
     print(e)
